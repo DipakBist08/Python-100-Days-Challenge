5dee0f05cd9d615ca60265af60ab0c8f91119b1d
Dipak Bist <mbdds54@gmail.com>
2024-08-24 12:11:42 +0545

    sum of even numbers from 1 to 100

---
+++ Day5/sum_of_even_numbers_1_100.py
@@ -0,0 +1,7 @@
+from Day5.Iterate_number_1_100 import number
+
+Total_Sum=0
+for number in range(1,101):
+    if number % 2 == 0:
+        Total_Sum=number
+print(f"Sum of even numbers 1 to 100 is :{Total_Sum}")
