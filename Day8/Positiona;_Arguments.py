d90f4801cc269af2c0a2a395b6fee9cd6ffc0f26
Dipak Bist <mbdds54@gmail.com>
2024-10-20 17:09:40 +0545

    Positional Arguments

---
+++ Day8/Positiona;_Arguments.py
@@ -0,0 +1,11 @@
+def My_Func(name,location,age):
+    print(f"Hello,{name}")
+    print(f"What is like in {location}")
+    print(f"Could you say your age:{age}")
+
+# My_Func("David","London",28)
+
+My_Func(name="David",location="New-York",age=29)
+
+#When you call a function, we have to assign  particular parameter with pace of data.
+
